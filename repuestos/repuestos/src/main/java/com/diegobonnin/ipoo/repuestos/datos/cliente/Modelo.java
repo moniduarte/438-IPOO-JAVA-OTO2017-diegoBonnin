6d43734695be065093af2a526afc7798a8faeba8
dbonnin <dbonnin@visionbanco.com>
2017-05-10 21:25:07 -0400

    Agregada la clase Modelo

---
+++ repuestos/repuestos/src/main/java/com/diegobonnin/ipoo/repuestos/datos/cliente/Modelo.java
@@ -0,0 +1,39 @@
+package com.diegobonnin.ipoo.repuestos.datos.cliente;
+
+public class Modelo {
+	
+	public int id;
+	public String nombre;
+	public String descripcion;
+	
+	public Modelo(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+
+}
